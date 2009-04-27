de50e0e5897a8c0ac196d44ac1f31349e431d73a
Christophe Clapp <christophe@christophe.cc>
2009-04-27 06:58:20 +0000

    Created GeolocalisationResult class and its builder.

---
+++ client/src/main/java/com/gisgraphy/client/domain/GeolocalisationResult.java
@@ -0,0 +1,264 @@
+package com.gisgraphy.client.domain;
+
+public class GeolocalisationResult {
+	private Double distance;
+	private String name;
+	private String adm1Code;
+	private String adm2Code;
+	private String adm3Code;
+	private String adm4Code;
+	private String adm1Name;
+	private String adm2Name;
+	private String adm3Name;
+	private String adm4Name;
+	private String asciiName;
+	private String countryCode;
+	private String featureClass;
+	private String featureCode;
+	private Integer featureId;
+	private Integer gTopo30;
+	private Integer population;
+	private String timezone;
+	private Double latitude;
+	private Double longitude;
+	private String placeType;
+	private String zipCode;
+	private String googleMapUrl;
+	private String yahooMapUrl;
+	private String countryFlagUrl;
+	
+	public class GeolocalisationResultBuilder {
+		private GeolocalisationResult result;
+		
+		public GeolocalisationResultBuilder() {
+			this.result = new GeolocalisationResult();
+		}
+		
+		public GeolocalisationResultBuilder withDistance(Double distance) {
+			result.distance = distance;
+			return this;
+		}
+		
+		public GeolocalisationResultBuilder withName(String name) {
+			result.name = name;
+			return this;
+		}
+		
+		public GeolocalisationResultBuilder withAdm1Code(String adm1Code) {
+			result.adm1Code = adm1Code;
+			return this;
+		}
+		
+		public GeolocalisationResultBuilder withAdm2Code(String adm2Code) {
+			result.adm2Code = adm2Code;
+			return this;
+		}
+		
+		public GeolocalisationResultBuilder withAdm3Code(String adm3Code) {
+			result.adm3Code = adm3Code;
+			return this;
+		}
+		
+		public GeolocalisationResultBuilder withAdm4Code(String adm4Code) {
+			result.adm4Code = adm4Code;
+			return this;
+		}
+		
+		public GeolocalisationResultBuilder withAdm1Name(String adm1Name) {
+			result.adm1Name = adm1Name;
+			return this;
+		}
+		
+		public GeolocalisationResultBuilder withAdm2Name(String adm2Name) {
+			result.adm2Name = adm2Name;
+			return this;
+		}
+		
+		public GeolocalisationResultBuilder withAdm3Name(String adm3Name) {
+			result.adm3Name = adm3Name;
+			return this;
+		}
+		
+		public GeolocalisationResultBuilder withAdm4Name(String adm4Name) {
+			result.adm4Name = adm4Name;
+			return this;
+		}
+		
+		public GeolocalisationResultBuilder withAsciiName(String asciiName) {
+			result.asciiName = asciiName;
+			return this;
+		}
+		
+		public GeolocalisationResultBuilder withCountryCode(String countryCode) {
+			result.countryCode = countryCode;
+			return this;
+		}
+		
+		public GeolocalisationResultBuilder withFeatureClass(String featureClass) {
+			result.featureClass = featureClass;
+			return this;
+		}
+		
+		public GeolocalisationResultBuilder withFeatureCode(String featureCode) {
+			result.featureCode = featureCode;
+			return this;
+		}
+		
+		public GeolocalisationResultBuilder withFeatureId(Integer featureId) {
+			result.featureId = featureId;
+			return this;
+		}
+		
+		public GeolocalisationResultBuilder withGTopo30(Integer gTopo30) {
+			result.gTopo30 = gTopo30;
+			return this;
+		}
+		
+		public GeolocalisationResultBuilder withPopulation(Integer population) {
+			result.population = population;
+			return this;
+		}
+		
+		public GeolocalisationResultBuilder withTimezone(String timezone) {
+			result.timezone = timezone;
+			return this;
+		}
+		
+		public GeolocalisationResultBuilder withLatitude(Double latitude) {
+			result.latitude = latitude;
+			return this;
+		}
+		
+		public GeolocalisationResultBuilder withLongitude(Double longitude) {
+			result.longitude = longitude;
+			return this;
+		}
+		
+		public GeolocalisationResultBuilder withPlaceType(String placeType) {
+			result.placeType = placeType;
+			return this;
+		}
+		
+		public GeolocalisationResultBuilder withZipCode(String zipCode) {
+			result.zipCode = zipCode;
+			return this;
+		}
+		
+		public GeolocalisationResultBuilder withGoogleMapUrl(String googleMapUrl) {
+			result.googleMapUrl = googleMapUrl;
+			return this;
+		}
+		
+		public GeolocalisationResultBuilder withYahooMapUrl(String yahoooMapUrl) {
+			result.yahooMapUrl = yahooMapUrl;
+			return this;
+		}
+		
+		public GeolocalisationResultBuilder withCountryFlagUrl(String countryFlagUrl) {
+			result.countryFlagUrl = countryFlagUrl;
+			return this;
+		}
+	}
+
+	public Double getDistance() {
+		return distance;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getAdm1Code() {
+		return adm1Code;
+	}
+
+	public String getAdm2Code() {
+		return adm2Code;
+	}
+
+	public String getAdm3Code() {
+		return adm3Code;
+	}
+
+	public String getAdm4Code() {
+		return adm4Code;
+	}
+
+	public String getAdm1Name() {
+		return adm1Name;
+	}
+
+	public String getAdm2Name() {
+		return adm2Name;
+	}
+
+	public String getAdm3Name() {
+		return adm3Name;
+	}
+
+	public String getAdm4Name() {
+		return adm4Name;
+	}
+
+	public String getAsciiName() {
+		return asciiName;
+	}
+
+	public String getCountryCode() {
+		return countryCode;
+	}
+
+	public String getFeatureClass() {
+		return featureClass;
+	}
+
+	public String getFeatureCode() {
+		return featureCode;
+	}
+
+	public Integer getFeatureId() {
+		return featureId;
+	}
+
+	public Integer getGTopo30() {
+		return gTopo30;
+	}
+
+	public Integer getPopulation() {
+		return population;
+	}
+
+	public String getTimezone() {
+		return timezone;
+	}
+
+	public Double getLatitude() {
+		return latitude;
+	}
+
+	public Double getLongitude() {
+		return longitude;
+	}
+
+	public String getPlaceType() {
+		return placeType;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public String getGoogleMapUrl() {
+		return googleMapUrl;
+	}
+
+	public String getYahooMapUrl() {
+		return yahooMapUrl;
+	}
+
+	public String getCountryFlagUrl() {
+		return countryFlagUrl;
+	}
+	
+	
+}
