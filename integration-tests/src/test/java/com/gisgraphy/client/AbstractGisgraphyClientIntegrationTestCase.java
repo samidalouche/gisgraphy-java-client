760519056bb558b4cdb3fcdd00deb1fee79fb14b
Christophe Clapp <christophe@christophe.cc>
2009-05-15 07:04:13 +0000

    Added tests for alternate names

--- integration-tests/src/test/java/com/gisgraphy/client/AbstractGisgraphyClientIntegrationTestCase.java
+++ integration-tests/src/test/java/com/gisgraphy/client/AbstractGisgraphyClientIntegrationTestCase.java
@@ -5,8 +5,10 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 
 import com.gisgraphy.client.impl.HttpGisgraphyServer;
+import org.junit.Ignore;
 
 @ContextConfiguration(locations = { "classpath:/com/gisgraphy/client/tests/integration/appcontext.xml"})
+@Ignore
 public class AbstractGisgraphyClientIntegrationTestCase extends AbstractJUnit4SpringContextTests {
 	@Autowired
 	protected HttpGisgraphyServer httpGisgraphyServer;
