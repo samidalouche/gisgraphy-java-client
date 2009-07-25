5a20f33260e850fd55634b7070dee918968d41e1
Christophe Clapp <christophe@christophe.cc>
2009-07-25 19:53:54 +0000

    Refactored

--- client/src/test/java/com/gisgraphy/client/domain/GisFeatureObjectMother.java
+++ client/src/test/java/com/gisgraphy/client/domain/GisFeatureObjectMother.java
@@ -5,13 +5,30 @@ package com.gisgraphy.client.domain;
  * @author christophe
  */
 public class GisFeatureObjectMother {
+    public static final String ANTARCTICA_FEATURE_CLASS = "A";
+    public static final String ANTARCTICA_FEATURE_CODE = "PCLI";
+    public static final long ANTARCTICA_FEATURE_ID = 661485L;
+    public static final String ANTARCTICA_FEATURE_NAME = "Antarctica";
+    public static final double ANTARCTICA_LATITUDE = 0.0;
+    public static final double ANTARCTICA_LONGITUDE = 0.0;
+    public static final long ANTARCTICA_POPULATION = 0L;
+    
     public static final String FRANCE_FEATURE_CODE = "PCLI";
     public static final long FRANCE_FEATURE_ID = 3017382L;
     public static final String FRANCE_FEATURE_NAME = "France";
     public static final String FRANCE_FEATURE_CLASS = "A";
-
     public static final double FRANCE_LONGITUDE = 2.0;
     public static final double FRANCE_LATITUDE = 46.0;
+    public static final long FRANCE_POPULATION = 64094000L;
+
+    public static final String US_OUTLYING_ISLANDS_FEATURE_CLASS = "A";
+    public static final String US_OUTLYING_ISLANDS_FEATURE_CODE = "ADMD";
+    public static final long US_OUTLYING_ISLANDS_FEATURE_ID = 5854968L;
+    public static final String US_OUTLYING_ISLANDS_FEATURE_NAME = "United States Minor Outlying Islands";
+    public static final long US_OUTLYING_ISLANDS_GTOPO30 = -9999L;
+    public static final double US_OUTLYING_ISLANDS_LONGITUDE = -162.072494506836;
+    public static final double US_OUTLYING_ISLANDS_LATITUDE = 5.88111019134521;
+    public static final long US_OUTLYING_ISLANDS_POPULATION = 0L;
 
     /**
      *
@@ -24,19 +41,19 @@ public class GisFeatureObjectMother {
                 .featureType(new GisFeatureType(FRANCE_FEATURE_CLASS,FRANCE_FEATURE_CODE))
                 .featureGeography(
                     GisFeatureGeography.gisFeatureGeography(FRANCE_LONGITUDE, FRANCE_LATITUDE)
-                    .population(64094000L)
+                    .population(FRANCE_POPULATION)
                     .build())
                 .build();
     }
 
     public static GisFeature antarcticaGisFeature() {
         return GisFeature.gisFeature()
-                .featureId(661485L)
-                .featureName(GisFeatureName.name("Antarctica"))
-                .featureType(new GisFeatureType("A", "PCLI"))
+                .featureId(ANTARCTICA_FEATURE_ID)
+                .featureName(GisFeatureName.name(ANTARCTICA_FEATURE_NAME))
+                .featureType(new GisFeatureType(ANTARCTICA_FEATURE_CLASS,ANTARCTICA_FEATURE_CODE))
                 .featureGeography(
-                    GisFeatureGeography.gisFeatureGeography(0.0, 0.0)
-                    .population(0L)
+                    GisFeatureGeography.gisFeatureGeography(ANTARCTICA_LONGITUDE,ANTARCTICA_LATITUDE)
+                    .population(ANTARCTICA_POPULATION)
                     .build())
                 .build();
     }
@@ -46,6 +63,15 @@ public class GisFeatureObjectMother {
      * @return
      */
     public static GisFeature usOutlyingIslandsGisFeature() {
-        throw new UnsupportedOperationException();
+        return GisFeature.gisFeature()
+                .featureId(US_OUTLYING_ISLANDS_FEATURE_ID)
+                .featureName(GisFeatureName.name(US_OUTLYING_ISLANDS_FEATURE_NAME))
+                .featureType(new GisFeatureType(US_OUTLYING_ISLANDS_FEATURE_CLASS,US_OUTLYING_ISLANDS_FEATURE_CODE))
+                .featureGeography(
+                    GisFeatureGeography.gisFeatureGeography(US_OUTLYING_ISLANDS_LONGITUDE, US_OUTLYING_ISLANDS_LATITUDE)
+                    .population(US_OUTLYING_ISLANDS_POPULATION)
+                    .gtopo30AverageElevation(US_OUTLYING_ISLANDS_GTOPO30)
+                    .build())
+                .build();
     }
 }
