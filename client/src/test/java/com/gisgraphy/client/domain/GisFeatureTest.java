b0d564ac7a37b37409af9ebc85448e565ad96e38
Sami Dalouche <sami.dalouche@gmail.com>
2009-07-28 23:34:10 +0000

    why .doubleValue() on a Long to compare it ?

--- client/src/test/java/com/gisgraphy/client/domain/GisFeatureTest.java
+++ client/src/test/java/com/gisgraphy/client/domain/GisFeatureTest.java
@@ -21,7 +21,7 @@ public class GisFeatureTest {
 	GisFeature france = GisFeatureObjectMother.franceGisFeature();
     assertEquals(france.getFeatureClass(), GisFeatureObjectMother.FRANCE_FEATURE_CLASS);
     assertEquals(france.getFeatureCode(), GisFeatureObjectMother.ANTARCTICA_FEATURE_CODE);
-    assertEquals(france.getFeatureId().doubleValue(), GisFeatureObjectMother.FRANCE_FEATURE_ID, 0.0001);
+    assertEquals(france.getFeatureId().longValue(), GisFeatureObjectMother.FRANCE_FEATURE_ID);
     assertEquals(france.getName().getName(), GisFeatureObjectMother.FRANCE_FEATURE_NAME);
     assertNotNull(france.getGeography());
     assertEquals(france.getGeography().getLocation().getCoordinate().x, GisFeatureObjectMother.FRANCE_LONGITUDE, 0.0001);
