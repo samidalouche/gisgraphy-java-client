2b2dd1d2cca7064992af6ee475a939a7b0080b27
Christophe Clapp <christophe@christophe.cc>
2009-07-30 06:11:40 +0000

    Added toString, equals, and hashCode

--- client/src/main/java/com/gisgraphy/client/domain/GisFeatureGeography.java
+++ client/src/main/java/com/gisgraphy/client/domain/GisFeatureGeography.java
@@ -2,6 +2,8 @@ package com.gisgraphy.client.domain;
 
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.Coordinate;
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
 
 /**
  *
@@ -87,5 +89,54 @@ class GisFeatureGeography {
 
     public String getTimeZone() {
         return timeZone;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final GisFeatureGeography other = (GisFeatureGeography) obj;
+        if (this.location != other.location && (this.location == null || !this.location.equals(other.location))) {
+            return false;
+        }
+        if (this.elevation != other.elevation && (this.elevation == null || !this.elevation.equals(other.elevation))) {
+            return false;
+        }
+        if (this.gtopo30AverageElevation != other.gtopo30AverageElevation && (this.gtopo30AverageElevation == null || !this.gtopo30AverageElevation.equals(other.gtopo30AverageElevation))) {
+            return false;
+        }
+        if (this.population != other.population && (this.population == null || !this.population.equals(other.population))) {
+            return false;
+        }
+        if ((this.timeZone == null) ? (other.timeZone != null) : !this.timeZone.equals(other.timeZone)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 59 * hash + (this.location != null ? this.location.hashCode() : 0);
+        hash = 59 * hash + (this.elevation != null ? this.elevation.hashCode() : 0);
+        hash = 59 * hash + (this.gtopo30AverageElevation != null ? this.gtopo30AverageElevation.hashCode() : 0);
+        hash = 59 * hash + (this.population != null ? this.population.hashCode() : 0);
+        hash = 59 * hash + (this.timeZone != null ? this.timeZone.hashCode() : 0);
+        return hash;
+    }
+
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
+                .append("location", this.location)
+                .append("elevation", this.elevation)
+                .append("gtopo30AverageElevation", this.gtopo30AverageElevation)
+                .append("population", this.population)
+                .append("timeZone", this.timeZone).toString();
     }
 }
--- client/src/main/java/com/gisgraphy/client/domain/GisFeatureType.java
+++ client/src/main/java/com/gisgraphy/client/domain/GisFeatureType.java
@@ -1,6 +1,8 @@
 package com.gisgraphy.client.domain;
 
 import org.apache.commons.lang.Validate;
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
 
 /**
  *
@@ -24,5 +26,36 @@ public class GisFeatureType {
 
     public String getFeatureCode() {
         return featureCode;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final GisFeatureType other = (GisFeatureType) obj;
+        if ((this.featureClass == null) ? (other.featureClass != null) : !this.featureClass.equals(other.featureClass)) {
+            return false;
+        }
+        if ((this.featureCode == null) ? (other.featureCode != null) : !this.featureCode.equals(other.featureCode)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 37 * hash + (this.featureClass != null ? this.featureClass.hashCode() : 0);
+        hash = 37 * hash + (this.featureCode != null ? this.featureCode.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE).append("featureClass", this.featureClass).append("featureCode", this.featureCode).toString();
     }
 }
