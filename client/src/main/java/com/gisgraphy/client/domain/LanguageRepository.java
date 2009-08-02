f423200d7260e29dcbf27ea64eccd6db00cc6077
Sami Dalouche <sami.dalouche@gmail.com>
2009-08-02 01:04:17 +0000

    cosmetic changes

--- client/src/main/java/com/gisgraphy/client/domain/LanguageRepository.java
+++ client/src/main/java/com/gisgraphy/client/domain/LanguageRepository.java
@@ -1,6 +1,6 @@
 package com.gisgraphy.client.domain;
 
 public interface LanguageRepository {
-    IsoLanguage findByAlpha3Code(String code);
     IsoLanguage findByAlpha2Code(String code);
+    IsoLanguage findByAlpha3Code(String code);
 }
