f1ca1d263dc3588d2dbcd17dba91365a26a06b33
Sami Dalouche <sami.dalouche@gmail.com>
2009-08-10 00:59:01 +0000

    added Javadoc

--- client/src/main/java/com/gisgraphy/client/administrativedivision/GeographicCountryInformation.java
+++ client/src/main/java/com/gisgraphy/client/administrativedivision/GeographicCountryInformation.java
@@ -1,6 +1,12 @@
 package com.gisgraphy.client.administrativedivision;
 
 public interface GeographicCountryInformation {
+    /**
+     * @return null or zero if information not available
+     */
     Long getPopulation();
+    /**
+     * @return null or zero if information not available
+     */
     Double getArea();
 }
